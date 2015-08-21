bf68e43660ab25ede9f01aad1d1a79ec6b64c6c6
bimgate <bimgate@gmail.com>
2015-08-21 16:16:15 +0200

    heroku config css2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,13 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  #PROMENJENO ZA HEROKU
+  #config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  config.serve_static_files = true
+
+  #PROMENJENO ZA HEROKU
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -76,10 +82,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-#OVO SAM DODAO ZA HEROKU
-  
-  config.serve_static_assets = true
-  
-#OVO SAM DODAO ZA HEROKU
 
 end
