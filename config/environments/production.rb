3897ec38c68b4bc7452ead18d97b208cb41be1c5
bimgate <bimgate@gmail.com>
2015-10-24 22:31:39 +0200

    heroku push21

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,16 +94,16 @@ config.action_mailer.default :charset => "utf-8"
 
 
 
-#config.action_mailer.delivery_method = :smtp
-#config.action_mailer.smtp_settings = {
- #   :address              => 'smtp.gmail.com',
- #   :port                 => 587,
- #   :domain               => 'heroku.com',
- #   :user_name            => 'bacvashop@gmail.com',
-  #  :password             => 'zicakv12',
-  #  :authentication       => 'plain',
-  #  :enable_starttls_auto => true
-#}
+
+config.action_mailer.smtp_settings = {
+   :address              => 'smtp.gmail.com',
+   :port                 => 587,
+   :domain               => 'gmail.com',
+   :user_name            => 'bacvashop@gmail.com',
+   :password             => 'zicakv12',
+   :authentication       => 'plain',
+   :enable_starttls_auto => true
+}
 
 
 end
