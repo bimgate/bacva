c5143ab8215617a80b39ecca085504e01d216f62
bimgate <bimgate@gmail.com>
2015-10-24 13:17:35 +0200

    heroku push5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,12 +83,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-ActionMailer::Base.delivery_method = :sendmail
-ActionMailer::Base.perform_deliveries = true
-ActionMailer::Base.raise_delivery_errors = true
-ActionMailer::Base.default_charset = "utf-8"
-
-
 config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :address              => 'smtp.gmail.com',
