5545cd18be9422e1404b14d34536b251ba3c54d7
bimgate <bimgate@gmail.com>
2015-10-24 13:22:10 +0200

    heroku push6

--- config/environment.rb
+++ config/environment.rb
@@ -7,5 +7,5 @@ Rails.application.initialize!
 ActionMailer::Base.delivery_method = :sendmail
 ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.raise_delivery_errors = true
-ActionMailer::Base.default_charset = "utf-8"
+
 
