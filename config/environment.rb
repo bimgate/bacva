fd7ef9f27eb218fd39d6907f2b1a2bfd9d9186b5
bimgate <bimgate@gmail.com>
2015-10-24 16:35:58 +0200

    heroku push9

--- config/environment.rb
+++ config/environment.rb
@@ -4,17 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.delivery_method = :sendmail
-ActionMailer::Base.perform_deliveries = true
-ActionMailer::Base.raise_delivery_errors = true
-
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => 'bacvashop@gmail.com',
-  :password       => 'zicakv12',
-  :domain         => 'heroku.com',
-  :enable_starttls_auto => true
-}
 
