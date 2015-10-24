8aed0c99706b96a055edd4a23175661d9f84b027
bimgate <bimgate@gmail.com>
2015-10-24 22:28:22 +0200

    heroku push20

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
+  :address        => 'smtp.gmail.com',
   :port           => '587',
   :authentication => :plain,
   :user_name      => 'bacvashop@gmail.com', #ENV['SENDGRID_USERNAME'],
   :password       => 'zicakv12',            #ENV['SENDGRID_PASSWORD'],
-  :domain         => 'heroku.com'
+  :domain         => 'gmail.com'
 }
 ActionMailer::Base.delivery_method = :smtp
