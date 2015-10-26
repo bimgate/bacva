0642945b88fa822ec029c1a583bd6dacb4853884
bimgate <bimgate@gmail.com>
2015-10-26 21:03:00 +0100

    style front

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -11,13 +11,13 @@ class StoreController < ApplicationController
 @products = Product.paginate(page: params[:page],per_page: 4).search(params[:search]).order("created_at DESC")
  
     elsif params[:price] && params[:price] == '1'
- @products = Product.paginate(page: params[:page],per_page: 4).where(price: 1000..2000)
+ @products = Product.paginate(page: params[:page],per_page: 4).where(price: 100..200)
 
     elsif params[:price] && params[:price] == '2'
- @products = Product.paginate(page: params[:page],per_page: 4).where(price: 2000..3000)
+ @products = Product.paginate(page: params[:page],per_page: 4).where(price: 200..300)
 
     elsif params[:price] && params[:price] == '3'
- @products = Product.paginate(page: params[:page],per_page: 4).where(price: 3000..5000)
+ @products = Product.paginate(page: params[:page],per_page: 4).where(price: 300..500)
     else
      
       @products = Product.paginate(page: params[:page],per_page: 4)
